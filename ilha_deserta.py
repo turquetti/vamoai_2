fe753a7f1b58e1dad4703900a65edcf934be02a6
turquetti <gabrielaturquetti@gmail.com>
2021-02-23 20:49:54 -0300

    Adding files

---
+++ ilha_deserta.py
@@ -0,0 +1,5 @@
+item1 = input("Qual seria o primeiro item que você levaria? ")
+item2 = input("Qual seria o segundo item que você levaria? ")
+item3 = input("Qual seria o terceiro item que você levaria? ")
+
+print(f"Você levaria para uma ilha deserta: {item1}, {item2}, {item3}.")
