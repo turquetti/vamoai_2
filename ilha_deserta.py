e6bf7ac19cf8c98c1367531b31d255513b164aa4
turquetti <gabrielaturquetti@gmail.com>
2021-02-23 20:50:49 -0300

    deleting ilha_deserta

--- ilha_deserta.py
+++
@@ -1,5 +0,0 @@
-item1 = input("Qual seria o primeiro item que você levaria? ")
-item2 = input("Qual seria o segundo item que você levaria? ")
-item3 = input("Qual seria o terceiro item que você levaria? ")
-
-print(f"Você levaria para uma ilha deserta: {item1}, {item2}, {item3}.")
